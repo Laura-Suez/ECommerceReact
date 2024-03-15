598bdf5e28ac19761216505652109d96e2db59d8
Laura Suez <laura.suez@outlook.com>
2024-03-14 21:52:50 -0300

    Clase10Check

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ItemDetailContainer } from "./components/pages/itemDetailContainer/ItemDetailContainer";
 import ItemListContainer from "./components/pages/ItemListContainer/ItemListContainer";
-import { Navbar } from "./components/layout/Navbar";
 import { CartContainer } from "./components/pages/Cart/CartContainer";
+import { Layout } from "./components/layout/Layout";
+import { CheckoutContainer } from "./components/pages/Cart/Checkout/CheckoutContainer";
 
 export const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
           <Route path="/" element={<ItemListContainer />} />
           <Route path="/category/:category" element={<ItemListContainer />} />
           <Route path="/cart" element={<CartContainer />} />
           <Route path="/item/:id" element={<ItemDetailContainer />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+          <Route path="/checkout" element={<CheckoutContainer />} />
+          <Route path="*" element={<h1>404 not found</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
-
 import CartWidget from "../common/CartWidget";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
